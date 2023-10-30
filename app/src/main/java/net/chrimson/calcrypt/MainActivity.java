b521b5e278cd0576d9b7778dc5fe252b2e038fe7
Chris Limson <chrimson@gmail.com>
2023-10-30 09:59:03 -0400

    Include addition

--- app/src/main/java/net/chrimson/calcrypt/MainActivity.java
+++ app/src/main/java/net/chrimson/calcrypt/MainActivity.java
@@ -8,13 +8,17 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private enum Op {
         ADD, SUB, MUL, DIV, MOD, POW, NON, GCD, PHI
     }
     private Op op;
 
-    private double firstValue;
+    private List<Double> values = new ArrayList<Double>();
+    private String valueText;
     private TextView display;
     private ScrollView scroller;
 
@@ -23,10 +27,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        valueText = "";
+        op = Op.NON;
+
         scroller = findViewById(R.id.scroller);
 
         display = findViewById(R.id.display);
 
+        //region Button Ids
         Button btn0 = findViewById(R.id.btn0);
         Button btn1 = findViewById(R.id.btn1);
         Button btn2 = findViewById(R.id.btn2);
@@ -37,21 +45,64 @@ public class MainActivity extends AppCompatActivity {
         Button btn7 = findViewById(R.id.btn7);
         Button btn8 = findViewById(R.id.btn8);
         Button btn9 = findViewById(R.id.btn9);
-        Button btnEqu = findViewById(R.id.equ);
+        Button btnAdd = findViewById(R.id.add);
+        Button btnEnt = findViewById(R.id.ent);
+        //endregion
 
+        //region Number Button Listeners
+        btn0.setOnClickListener(view -> {
+            valueText += "0";
+            display.append("0");
+        });
+        btn1.setOnClickListener(view -> {
+            valueText += "1";
+            display.append("1");
+        });
+        btn2.setOnClickListener(view -> {
+            valueText += "2";
+            display.append("2");
+        });
+        btn3.setOnClickListener(view -> {
+            valueText += "3";
+            display.append("3");
+        });
+        btn4.setOnClickListener(view -> {
+            valueText += "4";
+            display.append("4");
+        });
+        btn5.setOnClickListener(view -> {
+            valueText += "5";
+            display.append("5");
+        });
+        btn6.setOnClickListener(view -> {
+            valueText += "6";
+            display.append("6");
+        });
+        btn7.setOnClickListener(view -> {
+            valueText += "7";
+            display.append("7");
+        });
+        btn8.setOnClickListener(view -> {
+            valueText += "8";
+            display.append("8");
+        });
+        btn9.setOnClickListener(view -> {
+            valueText += "9";
+            display.append("9");
+        });
+        //endregion
 
-        btn0.setOnClickListener(view -> display.append("0"));
-        btn1.setOnClickListener(view -> display.append("1"));
-        btn2.setOnClickListener(view -> display.append("2"));
-        btn3.setOnClickListener(view -> display.append("3"));
-        btn4.setOnClickListener(view -> display.append("4"));
-        btn5.setOnClickListener(view -> display.append("5"));
-        btn6.setOnClickListener(view -> display.append("6"));
-        btn7.setOnClickListener(view -> display.append("7"));
-        btn8.setOnClickListener(view -> display.append("8"));
-        btn9.setOnClickListener(view -> display.append("9"));
+        btnAdd.setOnClickListener(view -> {
+            op = Op.ADD;
+            values.add(Double.parseDouble(valueText));
+            display.append("+");
+            valueText = "";
+        });
 
-        btnEqu.setOnClickListener(view -> {
+        btnEnt.setOnClickListener(view -> {
+            values.add(Double.parseDouble(valueText));
+
+            valueText = "";
             display.append("\n");
             scroller.fullScroll(View.FOCUS_DOWN);
         });
